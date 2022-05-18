9c96dadaa7d674ef5e546f580f8de2c9b3d25d4c
brdhub <2432152240@qq.com>
2022-05-18 11:51:39 +0800

    修复GetHot

--- src/main/java/com/www/util/GetHot.java
+++ src/main/java/com/www/util/GetHot.java
@@ -7,7 +7,7 @@ import java.net.URL;
 
 public class GetHot {
     public InputStream get(int page) throws IOException { //获取最热门的inputStream
-        String website = "https://api.github.com/search/repositories?q=language:java+created:%3E2010&sort=watchers&per_page=100&page=" + page;
+        String website = "https://api.github.com/search/repositories?q=language:java+created:>2010&sort=watchers&per_page=100&page=" + page;
         URL url = new URL(website);
         HttpURLConnection huc = (HttpURLConnection) url.openConnection();
         huc.setRequestMethod("GET");
