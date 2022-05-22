546fdea41092f73f36cc526fe28fb8554ec21126
brdhub <2432152240@qq.com>
2022-05-22 23:07:07 +0800

    添加词频分析

--- src/main/java/com/Java2Project/util/GetHottest.java
+++ src/main/java/com/Java2Project/util/GetHottest.java
@@ -21,7 +21,6 @@ public class GetHottest { //获取最热门的开源项目
             BufferedReader in = new BufferedReader(new InputStreamReader(gh.get(i)));
             String info = in.readLine();
             JSONObject jo1 = JSONObject.fromObject(info);
-            System.out.println(jo1.get("incomplete_results"));
             JSONArray ja = jo1.getJSONArray("items");
             int len = ja.size();
             for (int j = 0; j < len; j++) {
@@ -63,6 +62,8 @@ public class GetHottest { //获取最热门的开源项目
         BufferedWriter json = new BufferedWriter(new FileWriter("src/main/java/com/Java2Project/dataSet/info.json"));
         json.write(JSON);
         json.close();
+        WordAnalyse wa = new WordAnalyse();
+        wa.analyse();
     }
 
     public static class ItemArray {
---
+++ src/main/java/com/Java2Project/util/WordAnalyse.java
@@ -0,0 +1,54 @@
+package com.Java2Project.util;
+
+import com.google.gson.Gson;
+import com.kennycason.kumo.WordFrequency;
+import com.kennycason.kumo.nlp.FrequencyAnalyzer;
+import com.kennycason.kumo.nlp.tokenizers.ChineseWordTokenizer;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class WordAnalyse {
+    public void analyse() throws IOException {
+        FrequencyAnalyzer frequencyAnalyzer = new FrequencyAnalyzer();
+        frequencyAnalyzer.setWordFrequenciesToReturn(600); //最大词频
+        frequencyAnalyzer.setMinWordLength(2);
+        frequencyAnalyzer.setWordTokenizer(new ChineseWordTokenizer());
+        List<WordFrequency> wordFrequencyList = frequencyAnalyzer.load("src/main/java/com/Java2Project/dataSet/description.txt");
+        wordFrequencyList.sort(Comparator.comparingInt(WordFrequency::getFrequency));
+        IArray iArray = new IArray();
+        for(WordFrequency wordFrequency : wordFrequencyList)
+            iArray.add(new Item(wordFrequency.getWord(), wordFrequency.getFrequency()));
+        String cloud = new Gson().toJson(iArray);
+        BufferedWriter out = new BufferedWriter(new FileWriter("src/main/java/com/Java2Project/dataSet/cloud.json"));
+        out.write(cloud);
+        out.close();
+        System.out.println("云图生成成功");
+    }
+
+    public static class IArray{
+        ArrayList<Item> itemArrayList;
+
+        public IArray(){
+            itemArrayList = new ArrayList<>();
+        }
+
+        public void add(Item item){
+            itemArrayList.add(item);
+        }
+    }
+
+    public static class Item{
+        String word;
+        int frequency;
+
+        public Item(String word, int frequency){
+            this.word = word;
+            this.frequency = frequency;
+        }
+    }
+}
